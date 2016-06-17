fe7f4b4790380d376b87da00e53280e38d120db1
taylanksen <tsen@cs.rochester.edu>
2016-06-17 14:33:07 -0400

    initial revision appears to be working

--- merge_master.py
+++ merge_master.py
@@ -11,8 +11,13 @@
 """
 import glob
 import os
-from sets import Set
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+'''
+If you want to set the logging level from a command-line option such as:
+  --log=INFO
+'''
 
 #------------------------------------------------------------------------
 def convert2mp4():
@@ -24,13 +29,18 @@ def convert2mp4():
         fname_mp4 = root + '.mp4'
         # if mp4 file already exists skip
         if(os.path.exists(fname_mp4)):
-          continue
-        print('converting ' + fname + ' to mp4')
+            continue
+        logging.info('converting ' + fname + ' to mp4')
         cmd = "ffmpeg -i " + fname + \
               ' -vcodec libx264 -pix_fmt yuv420p -profile:v baseline ' + \
               ' -preset slow -crf 22 -movflags +faststart ' + fname_mp4
         
         os.system(cmd)
+
+        #proc = subprocess.Popen(["cat", "/etc/services"], stdout=subprocess.PIPE, shell=True)
+        #(out, err) = proc.communicate()
+        #print("program output:", out)        
+        
         # TODO capture log
 
 #------------------------------------------------------------------------
@@ -43,9 +53,9 @@ def extract_wav():
         fname_wav = root + '.wav'
         # skip if wav file already exists
         if(os.path.exists(fname_wav)):
-          print('WARNING: ' + fname_wav + ' exists, skipping.\n')
-          continue
-        print('  extracting wav from ' + fname + '\n')
+            logging.warning(fname_wav + ' exists, skipping.\n')
+            continue
+        logging.info('  extracting wav from ' + fname + '\n')
         cmd = "ffmpeg -async 1 -i " + fname + ' ' + fname_wav
         
         os.system(cmd)
@@ -57,24 +67,29 @@ def extract_wav():
 def merge_wavs(wav_file1, wav_file2, wav_file_out):
     """ merges two wav files while preserving length """
 
-    print('  MERGING WAV FILES: ' + wav_file1 + ' and ' + wav_file2)
-    print(' --> ' + wave_file_out + '\n')
+    logging.info('  MERGING WAV FILES: ' + wav_file1 + ' and ' + wav_file2)
+    logging.info(' --> ' + wav_file_out + '\n')
 
-    if(!os.path.exists(wav_file1)):
-      raise ValueError(wav_file1 + " file does not exist.")
-    if(!os.path.exists(wav_file2)):
-      raise ValueError(wav_file2 + " file does not exist.")
-    if(!os.path.exists(wav_file_out)):
-      raise ValueError(wav_file_out + " file does not exist.")
+    if(not os.path.exists(wav_file1)):
+        raise ValueError(wav_file1 + " file does not exist.")
+    if(not os.path.exists(wav_file2)):
+        raise ValueError(wav_file2 + " file does not exist.")
+    if(os.path.exists(wav_file_out)):
+        logging.warning(wav_file_out + ' exists, skipping.\n')
+        return
 
-    cmd = 'ffmpeg  -i ' +  wave_file1 + ' -i $2 -filter_complex ' + \
-          'amerge -ac 2 ' + wave_file_out
+    cmd = 'ffmpeg  -i ' +  wav_file1 + ' -i ' + wav_file2 +' -filter_complex ' + \
+          'amerge -ac 2 ' + wav_file_out
     os.system(cmd)
     # TODO save log
 
 #------------------------------------------------------------------------
 def merge_mp4s(infile1, infile2, outfile):
     """ merges two mp4 file into a new side by side mp4 file """
+    
+    if(os.path.exists(outfile)):
+        logging.warning(outfile + ' exists, skipping.\n')
+        return
 
     cmd = 'ffmpeg  -i ' + infile1 + ' -i ' + infile2 + \
           ' -filter_complex "[0:v]setpts=PTS-STARTPTS, pad=iw*2:ih[bg]; [1:v]setpts=PTS-STARTPTS[fg]; [bg][fg]overlay=w;  amerge, pan=stereo:c0<c0+c2:c1<c1+c3" ' + outfile
@@ -92,48 +107,73 @@ def merge_mp4s(infile1, infile2, outfile):
 def remove_audio(infile, outfile):
     """ creates a new mp4 file without audio  """
 
-    if(!os.path.exists(infile)):
-      raise ValueError(infile + " file does not exist.")
+    if(os.path.exists(outfile)):
+        logging.warning(outfile + ' exists, skipping.\n')
+        return
+    
+    if( not os.path.exists(infile)):
+        raise ValueError(infile + " file does not exist.")
 
     cmd = 'ffmpeg -i ' + infile + ' -c copy -an ' + outfile
     os.system(cmd)
     # TODO save log
 
+#------------------------------------------------------------------------
+def merge_av(wav_file, mp4_file, outfile):
+    """ creates a new mp4 file with audio from wav and vid from mp4  """
+
+    if(os.path.exists(outfile)):
+        logging.warning(outfile + ' exists, skipping.\n')
+        return
+
+    if(not os.path.exists(wav_file)):
+        raise ValueError(wav_file + " file does not exist.")
+
+    if(not os.path.exists(mp4_file)):
+        raise ValueError(wav_file + " file does not exist.")
+
+    cmd = 'ffmpeg -i ' + wav_file + ' -i ' + mp4_file + \
+          ' -c:v libx264 -c:a libmp3lame -shortest ' + outfile
+    os.system(cmd)
+    # TODO save log
 
 #------------------------------------------------------------------------
 def pair_steps():
     """ Before: a directory of mp4 files and wav files """
    
     roots = set()
-    mp4_list = glob.glob('*.mp4')
+    mp4_files = glob.glob('*.mp4')
     for mp4_file in mp4_files:
         fname_split = mp4_file.split('-')
-        root = '-'.join(fsplit[0:6])
+        root = '-'.join(fname_split[0:6])
         roots.add(root)    
 
     for root in roots:
 
         # merge wav
-        wav_files = glob.glob(root + '*')
-        assert(len(wav_files == 2)
+        wav_files = glob.glob(root + '*.wav')
+        assert(len(wav_files) >= 2)
         merge_wavs(wav_files[0], wav_files[1], root + '.wav')
 
         # merge mp4
-        merge_mp4s()
+        mp4_files = glob.glob(root + '*.mp4')
+        assert(len(mp4_files) >= 2)
+        merge_mp4_fname = root + '-merged.mp4'
+        merge_mp4s(mp4_files[0], mp4_files[1], merge_mp4_fname)
 
         # remove audio from mp4
-        remove_audio()
+        merge_mp4_noa_fname = root + '-merged.noa.mp4'
+        remove_audio(merge_mp4_fname, merge_mp4_noa_fname)
 
         # merge merged audio and video
-        merge_av()
+        merge_av(root + '.wav', root + '-merged.noa.mp4', root + '.mp4')
 
 
 #------------------------------------------------------------------------
 def do_all():
     convert2mp4()
     extract_wav()
-    
-
+    pair_steps()
 
 #------------------------------------------------------------------------
 do_all()
